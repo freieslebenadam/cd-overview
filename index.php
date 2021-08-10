1553c3729eb2e2ee54ea3b3cb3ad7c4c3278c383
freieslebenadam <adam.fre@seznam.cz>
2021-08-10 11:15:23 +0200

    includes

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CD Evidence</title>
+    <?php require_once 'resources/inc/head.inc.html'; ?>
 </head>
 <body>
-    
+    <?php require_once 'resources/inc/cds.class.inc.php'; 
+    $cv = new CdsView();
+    $cds = $cv->getAllCds();
+    ?>
+    <?php foreach ($cds as $cd): ?>
+    <div>
+        <img src="<?= $cd['Picture'] ?>" width="80px" />
+        <h2><?= $cd['Album'] ?></h2>
+        <h3><?= $cd['Author'] ?></h3>
+        <p><?= $cd['ReleaseDate'] ?></p>
+        <p><?= $cd['Genre'] ?></p>
+    </div>
+    <?php endforeach; ?>
 </body>
 </html>
---
+++ resources/inc/cds.class.inc.php
@@ -0,0 +1,8 @@
+<?php
+// Database Class
+require_once 'resources/class/database.class.php';
+
+// CDs Model Class
+require_once 'resources/class/cds.model.class.php';
+// CDs View Class
+require_once 'resources/class/cds.view.class.php';
